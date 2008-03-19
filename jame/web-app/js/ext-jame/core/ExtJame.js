db88a9bca06351e27ea4beedbd896a0f2ab0e4ee
Robert Schönthal <caziel@gmx.net>
2008-03-19 14:23:06 +0000

    fixed space bug in htmleditor via overide

--- jame/web-app/js/ext-jame/core/ExtJame.js
+++ jame/web-app/js/ext-jame/core/ExtJame.js
@@ -53,40 +53,15 @@ Ext.onReady(function(){
     	ExtJame.hud.on('click', ExtJame.init);
 });
 
-/**
- * provides scrollTo for panels
- */
-Ext.override(Ext.Element, {
-    scrollTo : function(side, value, animate){
-    	var side = side.toLowerCase();
-        var prop;
-    	switch (side) {
-    		case "left":
-    			prop = "scrollLeft";
-    			break;
-    		case "right":
-    			prop = "scrollLeft";
-    			value = this.dom.scrollWidth - (value + this.dom.clientWidth);
-    			break;
-    		case "top":
-    			prop = "scrollTop";
-    			break;
-    		case "bottom":
-    			prop = "scrollTop";
-    			value = this.dom.scrollHeight - (value + this.dom.clientHeight);
-    			break;
-    	}
-    	if (value < 0) value = 0;
-        if(!animate || !A){
-            this.dom[prop] = value;
-        }else{
-            var to = prop == "scrollLeft" ? [value, this.dom.scrollTop] : [this.dom.scrollLeft, value];
-            this.anim({scroll: {"to": to}}, this.preanim(arguments, 2), 'scroll');
+
+Ext.override(Ext.form.HtmlEditor, {
+    pushValue : function(){
+        if(this.initialized){
+            var v = this.el.dom.value;
+            if(this.fireEvent('beforepush', this, v) !== false){
+                (this.doc.body || this.doc.documentElement).innerHTML = v;
+                this.fireEvent('push', this, v);
+            }
         }
-        return this;
     }
 });
-
-window.onunload = function(){
-	//ExtJame.backend.Connection.loadXmlFromUrl(ExtJame.backend.url.logout, function(){});	
-}
